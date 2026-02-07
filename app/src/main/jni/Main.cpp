b10551894574bffcaf04bfe2e884a57079e0fc2d
akilltheboy <nofaceshow0@gmail.com>
2026-02-07 22:06:47 +0200

    Update Main.cpp

--- app/src/main/jni/Main.cpp
+++ app/src/main/jni/Main.cpp
@@ -192,6 +192,52 @@ void Changes(JNIEnv *env, jclass clazz, jobject obj, jint featNum, jstring featN
 // Hook Functions
 // ======================================
 
+// Hook SmartFox.Send to intercept ALL server requests
+void (*old_SmartFox_Send)(void* smartFox, void* request);
+void SmartFox_Send_Hook(void* smartFox, void* request) {
+    LOGI("=== SmartFox.Send INTERCEPTED ===");
+    LOGI("SmartFox: %p, Request: %p", smartFox, request);
+    
+    if (request != nullptr) {
+        // BaseRequest has 'id' field (short) at offset 0x10
+        // and 'targetController' at offset 0x18
+        short requestId = *(short*)((uintptr_t)request + 0x10);
+        LOGI("Request ID: %d", requestId);
+        
+        // Try to read more details from request object
+        // Check if it's ExtensionRequest (has extCmd field at offset 0x28)
+        void* possibleString = *(void**)((uintptr_t)request + 0x28);
+        if (possibleString != nullptr) {
+            // Il2CppString structure: object header (16 bytes) + length (4 bytes) + chars
+            int strLen = *(int*)((uintptr_t)possibleString + 0x10);
+            if (strLen > 0 && strLen < 256) {
+                char16_t* chars = (char16_t*)((uintptr_t)possibleString + 0x14);
+                char cmdBuf[257] = {0};
+                for (int i = 0; i < strLen && i < 256; i++) {
+                    cmdBuf[i] = (char)chars[i];
+                }
+                LOGI("Possible Command: %s", cmdBuf);
+            }
+        }
+        
+        // Log request types for reference:
+        // 0=Handshake, 1=Login, 7=GenericMessage, 13=CallExtension, 20=PublicMessage
+        switch(requestId) {
+            case 0: LOGI("Type: Handshake"); break;
+            case 1: LOGI("Type: Login"); break;
+            case 7: LOGI("Type: GenericMessage"); break;
+            case 13: LOGI("Type: CallExtension (ExtensionRequest)"); break;
+            case 20: LOGI("Type: PublicMessage"); break;
+            default: LOGI("Type: Unknown (%d)", requestId); break;
+        }
+    }
+    
+    LOGI("=== END INTERCEPT ===");
+    
+    // Call original
+    old_SmartFox_Send(smartFox, request);
+}
+
 // Hook Update to capture AttackComponent instance
 void (*old_Update)(void *instance);
 void Update(void *instance) {
@@ -537,6 +583,10 @@ void *hack_thread(void *) {
         il2cpp_object_new = (il2cpp_object_new_t)dlsym(il2cppHandle, "il2cpp_object_new");
         LOGI("il2cpp_object_new at: %p", il2cpp_object_new);
     }
+    
+    // Hook SmartFox.Send to intercept ALL requests!
+    HOOK(targetLibName, RVA_SMARTFOX_SEND, SmartFox_Send_Hook, old_SmartFox_Send);
+    LOGI("SmartFox.Send hooked!");
     
     // Hook Update
     HOOK(targetLibName, RVA_ATTACK_COMPONENT_UPDATE, Update, old_Update);
