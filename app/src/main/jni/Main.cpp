0f80c2acfd667d7cf2d1e6336a89c1bee0e9f597
akilltheboy <nofaceshow0@gmail.com>
2026-02-07 10:28:39 +0200

    Update Main.cpp

--- app/src/main/jni/Main.cpp
+++ app/src/main/jni/Main.cpp
@@ -152,52 +152,42 @@ void Update(void *instance) {
             LOGI("PlayerAttackComponent captured: %p", instance);
         }
         
-        // Handle Item Spawning - SEARCH ALL ITEMS
+        // Handle Item Spawning - TRUE SPAWNER
         if (spawnItemPressed) {
             spawnItemPressed = false;
-            LOGI("=== ITEM SPAWN ===");
+            LOGI("=== TRUE ITEM SPAWN ===");
             LOGI("Target item: %s", itemNameToSpawn.c_str());
-            LOGI("All items array: %p, count: %d", g_AllItemsArray, g_AllItemsCount);
             
-            void* foundItem = nullptr;
-            
-            // APPROACH 1: Search from g_AllItemsArray if available
-            if (g_AllItemsArray != nullptr && g_AllItemsCount > 0 && Il2CppStringNew != nullptr) {
-                void** allItems = (void**)((uintptr_t)g_AllItemsArray + 0x20);
+            // LAZY LOAD: Get all BadNerdItems on first spawn (game must be loaded)
+            if (g_AllItemsArray == nullptr && il2cpp_domain_get != nullptr) {
+                LOGI("Loading all items for first time...");
                 
-                for (int i = 0; i < g_AllItemsCount && i < 2000; i++) {
-                    void* item = allItems[i];
-                    if (item == nullptr) continue;
+                void* domain = il2cpp_domain_get();
+                if (domain != nullptr) {
+                    size_t assemblyCount = 0;
+                    void** assemblies = il2cpp_domain_get_assemblies(domain, &assemblyCount);
+                    LOGI("Found %zu assemblies", assemblyCount);
                     
-                    // Get item's GameObject name at offset 0x10 (UnityEngine.Object.m_CachedPtr)
-                    // Actually MonoBehaviour/Component stores name differently
-                    // BadNerdItem extends VNLObject which has obfuscated name
-                    // Let's try comparing gameObject.name through Component.gameObject.name
-                    
-                    // For debugging, log first 5 items
-                    if (i < 5) {
-                        LOGI("Item[%d]: %p", i, item);
-                    }
-                    
-                    // Try to get name - VNLObject has name via get_name() typically at some offset
-                    // Try offset 0x10 which is commonly the name string in il2cpp
-                    void* itemName = *(void**)((uintptr_t)item + 0x10);
-                    if (itemName != nullptr) {
-                        // Il2CppString: [0x10] = length, [0x14] = chars
-                        int nameLen = *(int*)((uintptr_t)itemName + 0x10);
-                        if (nameLen > 0 && nameLen < 100) {
-                            char16_t* nameChars = (char16_t*)((uintptr_t)itemName + 0x14);
-                            // Simple comparison for first chars
-                            bool match = true;
-                            for (size_t j = 0; j < itemNameToSpawn.length() && j < (size_t)nameLen; j++) {
-                                if ((char)nameChars[j] != itemNameToSpawn[j]) {
-                                    match = false;
-                                    break;
+                    for (size_t i = 0; i < assemblyCount; i++) {
+                        void* image = il2cpp_assembly_get_image(assemblies[i]);
+                        if (image != nullptr) {
+                            void* badNerdItemClass = il2cpp_class_from_name(image, "", "BadNerdItem");
+                            if (badNerdItemClass != nullptr) {
+                                LOGI("Found BadNerdItem class: %p", badNerdItemClass);
+                                
+                                void* type = il2cpp_class_get_type(badNerdItemClass);
+                                if (type != nullptr) {
+                                    void* typeObject = il2cpp_type_get_object(type);
+                                    LOGI("Type object: %p", typeObject);
+                                    
+                                    if (FindObjectsOfTypeAll != nullptr && typeObject != nullptr) {
+                                        g_AllItemsArray = FindObjectsOfTypeAll(typeObject);
+                                        if (g_AllItemsArray != nullptr) {
+                                            g_AllItemsCount = *(int*)((uintptr_t)g_AllItemsArray + 0x18);
+                                            LOGI("LOADED %d BadNerdItem objects!", g_AllItemsCount);
+                                        }
+                                    }
                                 }
-                            }
-                            if (match && itemNameToSpawn.length() <= (size_t)nameLen) {
-                                foundItem = item;
-                                LOGI("Found matching item at index %d: %p", i, item);
                                 break;
                             }
                         }
@@ -205,37 +195,75 @@ void Update(void *instance) {
                 }
             }
             
-            // APPROACH 2: Fallback to GetItemByName if item is in inventory
-            if (foundItem == nullptr && GetItemByName != nullptr && Il2CppStringNew != nullptr) {
-                void* nameStr = Il2CppStringNew(itemNameToSpawn.c_str());
-                foundItem = GetItemByName(instance, nameStr);
-                if (foundItem != nullptr) {
-                    LOGI("Found item in inventory: %p", foundItem);
+            // Search for item in loaded items
+            void* foundItem = nullptr;
+            
+            if (g_AllItemsArray != nullptr && g_AllItemsCount > 0) {
+                LOGI("Searching %d items for: %s", g_AllItemsCount, itemNameToSpawn.c_str());
+                void** allItems = (void**)((uintptr_t)g_AllItemsArray + 0x20);
+                
+                for (int i = 0; i < g_AllItemsCount && i < 3000; i++) {
+                    void* item = allItems[i];
+                    if (item == nullptr) continue;
+                    
+                    // VNLObject has a 'name' field - try common offsets
+                    // Try offset 0x10 (inherits from UnityEngine.Object which has name)
+                    void* itemNameStr = *(void**)((uintptr_t)item + 0x10);
+                    if (itemNameStr != nullptr) {
+                        int nameLen = *(int*)((uintptr_t)itemNameStr + 0x10);
+                        if (nameLen > 0 && nameLen < 100) {
+                            char16_t* nameChars = (char16_t*)((uintptr_t)itemNameStr + 0x14);
+                            
+                            // Debug: Log first 10 items
+                            if (i < 10) {
+                                char debugName[64] = {0};
+                                for (int k = 0; k < nameLen && k < 63; k++) {
+                                    debugName[k] = (char)nameChars[k];
+                                }
+                                LOGI("Item[%d]: %s", i, debugName);
+                            }
+                            
+                            // Check if matches
+                            bool match = true;
+                            if ((size_t)nameLen == itemNameToSpawn.length()) {
+                                for (size_t j = 0; j < itemNameToSpawn.length(); j++) {
+                                    if ((char)nameChars[j] != itemNameToSpawn[j]) {
+                                        match = false;
+                                        break;
+                                    }
+                                }
+                                if (match) {
+                                    foundItem = item;
+                                    LOGI("FOUND item at index %d!", i);
+                                    break;
+                                }
+                            }
+                        }
+                    }
                 }
+            } else {
+                LOGI("No items loaded! g_AllItemsArray=%p, count=%d", g_AllItemsArray, g_AllItemsCount);
             }
             
-            // Add item to inventory
+            // Add item to player inventory
             if (foundItem != nullptr) {
                 void* itemList = *(void**)((uintptr_t)instance + 0x138);
-                LOGI("itemList: %p", itemList);
+                LOGI("Adding to itemList: %p", itemList);
                 
                 if (itemList != nullptr) {
                     void* listItems = *(void**)((uintptr_t)itemList + 0x10);
                     int listSize = *(int*)((uintptr_t)itemList + 0x18);
                     
-                    LOGI("List _items: %p, _size: %d", listItems, listSize);
-                    
                     if (listItems != nullptr) {
                         int listCapacity = *(int*)((uintptr_t)listItems + 0x18);
-                        LOGI("Array capacity: %d", listCapacity);
                         
                         if (listSize < listCapacity) {
                             void** listItemsArray = (void**)((uintptr_t)listItems + 0x20);
                             listItemsArray[listSize] = foundItem;
                             *(int*)((uintptr_t)itemList + 0x18) = listSize + 1;
-                            LOGI("SUCCESS! Item added! New size: %d", listSize + 1);
+                            LOGI("SUCCESS! Item spawned! Size: %d", listSize + 1);
                         } else {
-                            LOGI("List is full! Size: %d, Capacity: %d", listSize, listCapacity);
+                            LOGI("Inventory full!");
                         }
                     }
                 }
@@ -292,7 +320,7 @@ void *hack_thread(void *) {
     LOGI(OBFUSCATE("%s has been loaded at base 0x%llX"), (const char *) targetLibName, (long long)g_il2cppELF.base());
 
 #if defined(__aarch64__)
-    // Get all il2cpp API functions using dlsym
+    // Get il2cpp API functions using dlsym
     void* il2cppHandle = dlopen("libil2cpp.so", RTLD_NOLOAD);
     if (il2cppHandle != nullptr) {
         Il2CppStringNew = (Il2CppStringNew_t)dlsym(il2cppHandle, "il2cpp_string_new");
@@ -303,56 +331,12 @@ void *hack_thread(void *) {
         il2cpp_assembly_get_image = (il2cpp_assembly_get_image_t)dlsym(il2cppHandle, "il2cpp_assembly_get_image");
         il2cpp_type_get_object = (il2cpp_type_get_object_t)dlsym(il2cppHandle, "il2cpp_type_get_object");
         
-        LOGI("il2cpp_string_new: %p", Il2CppStringNew);
-        LOGI("il2cpp_class_from_name: %p", il2cpp_class_from_name);
-        LOGI("il2cpp_domain_get: %p", il2cpp_domain_get);
-        
         // Get FindObjectsOfTypeAll from RVA
         FindObjectsOfTypeAll = (FindObjectsOfTypeAll_t)getAbsoluteAddress(targetLibName, 0x423CCE4);
-        LOGI("FindObjectsOfTypeAll: %p", FindObjectsOfTypeAll);
         
-        // Try to find BadNerdItem class
-        if (il2cpp_domain_get != nullptr && il2cpp_domain_get_assemblies != nullptr) {
-            void* domain = il2cpp_domain_get();
-            LOGI("Il2Cpp Domain: %p", domain);
-            
-            if (domain != nullptr) {
-                size_t assemblyCount = 0;
-                void** assemblies = il2cpp_domain_get_assemblies(domain, &assemblyCount);
-                LOGI("Assemblies count: %zu", assemblyCount);
-                
-                // Look for BadNerdItem in Assembly-CSharp
-                for (size_t i = 0; i < assemblyCount && il2cpp_class_from_name != nullptr; i++) {
-                    void* image = il2cpp_assembly_get_image(assemblies[i]);
-                    if (image != nullptr) {
-                        // Try to find BadNerdItem class (no namespace, root namespace)
-                        void* badNerdItemClass = il2cpp_class_from_name(image, "", "BadNerdItem");
-                        if (badNerdItemClass != nullptr) {
-                            LOGI("Found BadNerdItem class: %p in assembly %zu", badNerdItemClass, i);
-                            
-                            // Get the Type object for FindObjectsOfTypeAll
-                            if (il2cpp_class_get_type != nullptr && il2cpp_type_get_object != nullptr) {
-                                void* type = il2cpp_class_get_type(badNerdItemClass);
-                                if (type != nullptr) {
-                                    void* typeObject = il2cpp_type_get_object(type);
-                                    LOGI("BadNerdItem TypeObject: %p", typeObject);
-                                    
-                                    // Call FindObjectsOfTypeAll to get all items
-                                    if (FindObjectsOfTypeAll != nullptr && typeObject != nullptr) {
-                                        g_AllItemsArray = FindObjectsOfTypeAll(typeObject);
-                                        if (g_AllItemsArray != nullptr) {
-                                            g_AllItemsCount = *(int*)((uintptr_t)g_AllItemsArray + 0x18);
-                                            LOGI("Found %d BadNerdItem objects!", g_AllItemsCount);
-                                        }
-                                    }
-                                }
-                            }
-                            break;
-                        }
-                    }
-                }
-            }
-        }
+        LOGI("il2cpp_string_new: %p", Il2CppStringNew);
+        LOGI("il2cpp_domain_get: %p", il2cpp_domain_get);
+        LOGI("FindObjectsOfTypeAll: %p", FindObjectsOfTypeAll);
     } else {
         LOGE("Failed to get libil2cpp.so handle");
     }
